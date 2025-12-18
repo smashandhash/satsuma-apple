a30f3c59be4b951a699e965d2ca1a2fac9740304
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-12-18 22:12:34 +0700

    First commit

---
+++ satsuma/satsumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  satsumaApp.swift
+//  satsuma
+//
+//  Created by Gogo on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct satsumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
