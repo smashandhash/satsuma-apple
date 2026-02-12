1d9a96f6cfaa938175b40d3592e097fac0b4d0db
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-02-12 23:55:35 +0700

    In progress for creating the new chat form page.

--- satsuma/NewChatView.swift
+++ satsuma/NewChatView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct NewChatView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Search User", text: <#T##Binding<String>#>)
+        }
     }
 }
 
